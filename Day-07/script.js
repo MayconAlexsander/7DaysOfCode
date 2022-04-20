b90148064431b606c7b4d8738391851d66f3df76
MayconAlexsander <mayconalexm@gmail.com>
2022-04-20 14:50:55 -0300

    captured number button values

--- Day-07/script.js
+++ Day-07/script.js
@@ -1,7 +1,20 @@
+const number_0 = document.getElementById("botao-0").value
+const number_1 = document.getElementById("botao-1").value
+const number_2 = document.getElementById("botao-2").value
+const number_3 = document.getElementById("botao-3").value
+const number_4 = document.getElementById("botao-4").value
+const number_5 = document.getElementById("botao-5").value
+const number_6 = document.getElementById("botao-6").value
+const number_7 = document.getElementById("botao-7").value
+const number_8 = document.getElementById("botao-8").value
+const number_9 = document.getElementById("botao-9").value
+
+// Botão -> =
 function calcular() {
 
 }
 
+// Botão -> CE
 function apagarTudo() {
     
 }
